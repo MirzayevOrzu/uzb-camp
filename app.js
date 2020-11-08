e0d2eb9196fef06b2bae869410eb3089044af06b
Mirzayev Orzu <mirzayevorzu127@gmail.com>
2020-11-08 22:04:00 +0500

    add start tag

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = 'It seems like something went wrong';
     res.status(statusCode).render('error', { err })
 })
-
+const port = process.env.PORT || 3000;
 app.listen(3000, () => {
-    console.log('Listening on port 3000')
+    console.log(`Serving on port ${port}`)
 })
