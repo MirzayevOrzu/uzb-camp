9d8fd675df77939b19c86f3c45a61238f52345ce
Mirzayev Orzu <mirzayevorzu127@gmail.com>
2020-11-09 08:40:35 +0500

    customizing to uzb

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
     center: [65.54443359, 40.56389453],
-    zoom: 5
+    zoom: 3
 });
 
 map.addControl(new mapboxgl.NavigationControl());
@@ -21,7 +21,7 @@ map.on('load', function () {
         // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
         data: campgrounds,
         cluster: true,
-        clusterMaxZoom: 10, // Max zoom to cluster points on
+        clusterMaxZoom: 14, // Max zoom to cluster points on
         clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
     });
 
